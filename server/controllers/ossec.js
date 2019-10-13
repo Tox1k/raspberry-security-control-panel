3d467a7c69d1635f4d638b5c075a03c53e884d79
Tox1k <sashasolud@gmail.com>
2019-10-13 21:46:11 +0200

    log by lines

--- server/controllers/ossec.js
+++ server/controllers/ossec.js
@@ -1,11 +1,10 @@
 const shell = require('shelljs')
 
-const log = async ({ parameters }, res) => {
+const log = async ({ parameters: { lines } }, res) => {
   const path = '/var/ossec/logs/ossec.log'
-  const output = shell.tail({ '-n': 500 }, path)
-  console.log(output.reverse())
+  const output = shell.tail({ '-n': lines }, path)
   if (output.stderr) return res.status(400).json({ message: output.stderr })
-  return res.status(200).json({ log: output.stdout.reverse() })
+  return res.status(200).json({ log: output.stdout })
 }
 
 module.exports = {
