229b169f5bc51c12ccd036c471a129a1266d866d
Tox1k <sashasolud@gmail.com>
2019-10-13 21:29:28 +0200

    small fix

--- server/controllers/clamav.js
+++ server/controllers/clamav.js
@@ -2,7 +2,7 @@ const shell = require('shelljs')
 
 const log = async ({ parameters }, res) => {
   // const path = '/var/log/clamav/'
-  // const output = shell.tail('-500', path)
+  // const output = shell.tail(['-n', '500'], path)
   // console.log(output)
   // if (output.stderr) return res.status(400).json({ message: output.stderr })
   // return res.status(200).json({ log: output })
--- server/routes/api/v1/v1.js
+++ server/routes/api/v1/v1.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 // Routes
-// const suricata = require('./routes/suricata')
-// const ossec = require('./routes/ossec')
+const suricata = require('./routes/suricata')
+const ossec = require('./routes/ossec')
 const clamav = require('./routes/clamav')
 const services = require('./routes/services')
 
-// router.use('/suricata', suricata)
-// router.use('/ossec', ossec)
+router.use('/suricata', suricata)
+router.use('/ossec', ossec)
 router.use('/clamav', clamav)
 router.use('/services', services)
 
