444958a207aff84039413ba32d8b766b9a23e582
Tox1k <sashasolud@gmail.com>
2019-10-13 21:42:05 +0200

    testing

--- server/controllers/ossec.js
+++ server/controllers/ossec.js
@@ -3,9 +3,9 @@ const shell = require('shelljs')
 const log = async ({ parameters }, res) => {
   const path = '/var/ossec/logs/ossec.log'
   const output = shell.tail({ '-n': 500 }, path)
-  console.log(output)
+  console.log(output.reverse())
   if (output.stderr) return res.status(400).json({ message: output.stderr })
-  return res.status(200).json({ log: output })
+  return res.status(200).json({ log: output.stdout.reverse() })
 }
 
 module.exports = {
