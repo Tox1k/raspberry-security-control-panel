bb0d91d78b498ccec601d92dd8ca23d9d9e3847d
Tox1k <sashasolud@gmail.com>
2019-10-13 21:50:39 +0200

    params parsing

--- server/controllers/ossec.js
+++ server/controllers/ossec.js
@@ -1,6 +1,8 @@
 const shell = require('shelljs')
 
 const log = async ({ params: { lines } }, res) => {
+  lines = parseInt(lines, 10)
+  if (isNaN(lines) || lines <= 0) return res.status(400).json({ message: 'params error!' })
   const path = '/var/ossec/logs/ossec.log'
   const output = shell.tail({ '-n': lines }, path)
   if (output.stderr) return res.status(400).json({ message: output.stderr })
