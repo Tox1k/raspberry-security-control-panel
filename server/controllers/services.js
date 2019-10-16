4d9a7a7f5b02357065558074aa52b43cfa796f64
Tox1k <sashasolud@gmail.com>
2019-10-16 23:53:07 +0200

    logs

--- server/controllers/services.js
+++ server/controllers/services.js
@@ -2,6 +2,7 @@ const shell = require('shelljs')
 
 const execute = async ({ body: { command }, params: { service } }, res) => {
   service = service === 'antivirus' ? 'clamav' : service === 'ids' ? 'suricata' : service === 'collector' ? 'ossec' : service
+  console.log(service)
   if (service !== 'suricata' && service !== 'clamav' && service !== 'ossec') return res.status(400).json({ message: 'bad service!' })
   let output
   if (command !== 'start' && command !== 'stop' && command !== 'restart') return res.status(400).json({ message: 'bad command!' })
@@ -25,6 +26,7 @@ const execute = async ({ body: { command }, params: { service } }, res) => {
 
 const status = async ({ params: { service } }, res) => {
   service = service === 'antivirus' ? 'clamav' : service === 'ids' ? 'suricata' : service === 'collector' ? 'ossec' : service
+  console.log(service)
   if (service !== 'suricata' && service !== 'clamav' && service !== 'ossec') return res.status(400).json({ message: 'bad service!' })
   const output = shell.exec(`sudo service ${service} status`).stdout
   if (output.includes('Active: active (running)')) return res.status(200).json({ status: 'running' })
