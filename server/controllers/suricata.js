30c9861ccc9512c62441ad63a7290b4fa6333eb7
Tox1k <sashasolud@gmail.com>
2019-10-23 09:42:17 +0200

    suricata logs now

--- server/controllers/suricata.js
+++ server/controllers/suricata.js
@@ -3,7 +3,7 @@ const shell = require('shelljs')
 const log = async ({ query: { lines } }, res) => {
   lines = parseInt(lines, 10)
   if (isNaN(lines) || lines <= 0) return res.status(400).json({ message: 'params error!' })
-  const path = '/var/log/suricata/suricata.log'
+  const path = '/var/log/suricata/fast.log'
   const output = shell.tail({ '-n': lines }, path)
   console.log(output)
   if (output.stderr) return res.status(400).json({ message: output.stderr })
